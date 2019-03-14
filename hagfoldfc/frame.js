b800c6b81b86319142dd68b780abf662920e77d4
Gavin <gavin.gration@roar-it.com>
2019-03-14 13:48:17 +0000

    cc

--- hagfoldfc/frame.js
+++ hagfoldfc/frame.js
@@ -1,6 +1,3 @@
 window.addEventListener('beforeunload', function(event){
-  window.parent.postMessage('UNLOAD'+window.location.href, '*')
-})
-window.addEventListener('load', function(event){
-  window.parent.postMessage('LOAD'+window.location.href, '*')
+  window.parent.postMessage(window.location.href, '*')
 })
