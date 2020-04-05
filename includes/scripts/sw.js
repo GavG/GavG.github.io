91f36c03607240af3c13683d42b0a86743044455
GavG <gavingration@gmail.com>
2020-04-05 17:45:48 +0100

    SW update fetch cahces

--- includes/scripts/sw.js
+++ includes/scripts/sw.js
@@ -14,3 +14,33 @@ self.addEventListener('install', function(event) {
         })
     );
 });
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then(
+                function(response) {
+                    // Check if we received a valid response
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
+        })
+    );
+});
