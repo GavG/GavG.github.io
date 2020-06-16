a71bbeb416689784b5b3e009ed3128979f73f720
GavG <gavingration@gmail.com>
2020-06-16 18:19:18 +0100

    cookie consent test

--- includes/scripts/main.js
+++ includes/scripts/main.js
@@ -1,10 +1,3 @@
-window.ga = window.ga || function() {
-    (ga.q = ga.q || []).push(arguments)
-};
-ga.l = +new Date;
-ga('create', 'UA-XXXXX-Y', 'auto')
-ga('send', 'pageview')
-
 window.addEventListener('load', function() {
     var reveals = document.getElementsByClassName('reveals')
     for (i = 0; i < reveals.length; i++) {
@@ -17,4 +10,22 @@ window.addEventListener('load', function() {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.min.js')
     }
+
+    if (window.localStorage.getItem('cookie-consent')) {
+        window.ga = window.ga || function() {
+            (ga.q = ga.q || []).push(arguments)
+        };
+        ga.l = +new Date;
+        ga('create', 'UA-XXXXX-Y', 'auto')
+        ga('send', 'pageview')
+    } else {
+        window['ga-disable-UA-XXXXX-Y'] = true;
+        let cookie_div = document.getElementById('cookieWarning')
+        cookie_div.addEventListener('click', acceptCookies)
+        cookie_div.style.display = 'block'
+    }
 })
+
+function acceptCookies() {
+    window.localStorage.setItem('cookie-consent', 1)
+}
